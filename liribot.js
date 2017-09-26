fb93ad2ca3c9fd9fc72d0ee0bf8291ff125f2b20
kungge8 <kung.ge8@gmail.com>
2017-09-26 14:01:30 -0400

    some commenting

--- liribot.js
+++ liribot.js
@@ -5,6 +5,7 @@ var spot = require('./assets/spotify.js');
 var omdb = require('./assets/omdb.js');
 
 function random() {
+	//does the thing in random.txt
 	fs.readFile("random.txt", "utf8", function (error, data){
 		if(error){
 			console.log(error);
@@ -16,6 +17,7 @@ function random() {
 }
 
 function writeToLog(call, re){
+	//write the selected function and the output to log.txt
 	fs.appendFile("log.txt", "\n\n" + call + "\n" + re, function(err){
 			if(err){
 				console.log(err);
@@ -26,8 +28,11 @@ function writeToLog(call, re){
 }
 
 function decision(str, str2){
+	//controller for checking which function user wants to run
 	switch(str){
 	case ("my-tweets"):
+		//query twitter api for recent tweets for hardcoded account name because
+		// hw assignment says so
 		tweets.init(keys.twitterKeys);
 		tweets.getTweets().then(
 			function(val){
@@ -36,6 +41,7 @@ function decision(str, str2){
 		);
 		break;
 	case ("spotify-this-song"):
+		//query spotify for song based on track name
 		spot.init(keys.spotifyKeys);
 		spot.search(str2).then(
 			function (val){
@@ -47,6 +53,7 @@ function decision(str, str2){
 			});
 		break;
 	case ("movie-this"):
+		//query omdb for movie name
 		omdb.search(str2).then(
 			function (val){
 				writeToLog(str, omdb.write(val));
@@ -54,6 +61,7 @@ function decision(str, str2){
 		);
 		break;
 	case ("do-what-it-says"):
+		//do what it says in random.txts
 		writeToLog(str, random());
 		break;
 	}
